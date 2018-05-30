32c29f9da7b179520b41f9deef05d449cb6c43ce
MyLibh <gav503244@gmail.com>
2018-05-30 14:39:51 +0300

    Bubble sort as initial commit

---
+++ Algorithms/Sortings/BubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+
+/*
+=====================================================================
+
+Complexity = O(n ^ 2)
+
+Worst Case :
+The number of comparisons in the body of the cycle is(N - 1) * N / 2.
+The number of comparisons in the headers of cycles is(N - 1) * N / 2.
+The total number of comparisons is(N - 1) * N.
+The number of assignments in the cycle headers is(N - 1) * N / 2.
+The number of exchanges is(N - 1) * N / 2.
+
+Best case:
+The number of comparisons in the body of the cycle is(N - 1) * N / 2.
+The number of comparisons in the cycle headers is(N - 1) * N / 2.
+The total number of comparisons is(N - 1) * N.
+The number of exchanges is 0.
+
+=====================================================================
+*/
+
+template<typename Iter, typename Pred>
+void BubbleSort(Iter first, Iter last, Pred p)
+{
+	while (first < --last)
+		for (auto i{ first }; i < last; ++i)
+			if (p(*(i + 1), *i))
+				std::iter_swap(i, i + 1);
+}
