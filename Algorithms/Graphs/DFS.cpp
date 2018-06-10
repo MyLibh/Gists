09ff15a6eb459f4e79d9e2cb25c92b2c81deba95
MyLibh <gav503244@gmail.com>
2018-06-10 21:02:12 +0300

    DFS algorithm added

---
+++ Algorithms/Graphs/DFS.cpp
@@ -0,0 +1,27 @@
+#include <queue>     // std::queue
+#include <vector>    // std::vector
+#include <algorithm> // std::reverse
+
+using graph_t = std::vector<std::vector<long long>>;
+
+//====================================================================================================================================
+//!
+//! \brief   Realization of DFS(depth - first search) - a method of traversing the graph
+//!
+//! \param   graph  Graph
+//! \param   i      Node index
+//!
+//!			 O (N+M)
+//!
+//====================================================================================================================================
+
+void DFS(const graph_t &graph, size_t i)
+{
+	std::vector<char> used;
+	used[node_index] = true;
+
+	for (const auto& i : graph[i])
+		if (!used[i])
+			DFS(i);
+}
+
