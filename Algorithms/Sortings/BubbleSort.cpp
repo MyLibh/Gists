3d9670462be1a21a6cfa130710df701478bf7ee1
MyLibh <gav503244@gmail.com>
2018-06-18 22:10:43 +0300

    InsertionSort added

--- Algorithms/Sortings/BubbleSort.cpp
+++ Algorithms/Sortings/BubbleSort.cpp
@@ -1,30 +1,9 @@
-/*
-=====================================================================
-
-Complexity = O(n ^ 2)
-
-Worst Case :
-The number of comparisons in the body of the cycle is(N - 1) * N / 2.
-The number of comparisons in the headers of cycles is(N - 1) * N / 2.
-The total number of comparisons is(N - 1) * N.
-The number of assignments in the cycle headers is(N - 1) * N / 2.
-The number of exchanges is(N - 1) * N / 2.
-
-Best case:
-The number of comparisons in the body of the cycle is(N - 1) * N / 2.
-The number of comparisons in the cycle headers is(N - 1) * N / 2.
-The total number of comparisons is(N - 1) * N.
-The number of exchanges is 0.
-
-=====================================================================
-*/
-
 #include <utility> // std::iter_swap
 #include <xstddef> // std::less
 
 //====================================================================================================================================
 //!
-//! \brief  Sorts elements in [first, last) using pred
+//! \brief  Sorts elements in [first, last) using 'predicate'
 //!
 //! \param  first      Iterator to the begin of container
 //! \param  last       Iterator to the end of container
@@ -33,6 +12,9 @@ The number of exchanges is 0.
 //! \throw  ???
 //!
 //! \note   Uses std::less<> as default predicate
+//!
+//!          T(N) = O(N^2)
+//!			 M(N) = O(1)
 //!
 //====================================================================================================================================
 
@@ -44,3 +26,24 @@ void BubbleSort(Iterator first, Iterator last, Predicate predicate = Predicate()
 			if (predicate(*(i + 1), *i))
 				std::iter_swap(i, i + 1);
 }
+
+/*
+=====================================================================
+
+Complexity = O(N^2)
+
+Worst Case :
+The number of comparisons in the body of the cycle is (N - 1) * N / 2.
+The number of comparisons in the headers of cycles is (N - 1) * N / 2.
+The total number of comparisons is (N - 1) * N.
+The number of assignments in the cycle headers is (N - 1) * N / 2.
+The number of exchanges is (N - 1) * N / 2.
+
+Best case:
+The number of comparisons in the body of the cycle is (N - 1) * N / 2.
+The number of comparisons in the cycle headers is (N - 1) * N / 2.
+The total number of comparisons is (N - 1) * N.
+The number of exchanges is 0.
+
+=====================================================================
+*/
--- Algorithms/Sortings/InsertionSort.cpp
+++ Algorithms/Sortings/InsertionSort.cpp
@@ -1,2 +1,27 @@
-#include <algorithm>
+#include <utility> // std::iter_swap
+#include <xstddef> // std::less
 
+//====================================================================================================================================
+//!
+//! \brief  Sorts elements in [first, last) using 'predicate'
+//!
+//! \param  first      Iterator to the begin of container
+//! \param  last       Iterator to the end of container
+//! \param  predicate  Predicate to compare for sorting
+//!
+//! \throw  ???
+//!
+//! \note   Uses std::less<> as default predicate
+//!
+//!          T(N) = O(N^2)
+//!			 M(N) = O(1)
+//!
+//====================================================================================================================================
+
+template <typename Iterator, typename Predicate = std::less<>>
+void InsertionSort(Iterator first, Iterator last, Predicate pred = Predicate())
+{
+	for (Iterator i{ first + 1 }; i != last; ++i)
+		for (Iterator j{ i }; j != first && pred(*j, *(j - 1)); --j)
+			std::iter_swap(j - 1, j);
+}
