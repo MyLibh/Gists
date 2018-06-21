3463bcb3d5522c0cef5efa4bc4f48d726a28d0c7
MyLibh <gav503244@gmail.com>
2018-06-22 00:26:54 +0300

    Fixed BFS

--- Algorithms/Graphs/BFS.cpp
+++ Algorithms/Graphs/BFS.cpp
@@ -28,23 +28,23 @@ std::vector<T> BFS(const graph_t &graph, size_t num, T vertex, size_t start = 0u
 	std::vector<char> used(num);
 	used[start] = true;
 
-	std::vector<T> d(n),
-		           p(n);
-	p[s] = -1;
+	std::vector<T> d(num),
+		           p(num);
+	p[start] = -1;
 	while (!q.empty()) 
 	{
 		auto f{ q.front() };
 		q.pop();
 
 		for (auto i{ 0ull }; i < graph[f].size(); ++i)
-			if (auto t{ graph[f][i] }; !used[to])
+			if (auto t{ graph[f][i] }; !used[t])
 			{
 				used[t] = true;
 
 				q.push(t);
 
 				d[t] = d[f] + 1;
-				p[t] = v;
+				p[t] = f;
 			}
 	}
 
@@ -53,7 +53,7 @@ std::vector<T> BFS(const graph_t &graph, size_t num, T vertex, size_t start = 0u
 	else
 	{
 		std::vector<T> path;
-		for (T i = to; i != -1; i = p[i])
+		for (T i = vertex; i != -1; i = p[i])
 			path.push_back(v + 1);
 
 		std::reverse(path.begin(), path.end());
